0279450b56d16d91028b5e84664cc7d494fbbb58
Guillermo López García <lopezgarciaguillermo@live.com>
2021-07-07 01:17:43 +0200

    update home

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,8 +5,18 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <strong>
+        Guillermo López García
+      </strong>
+      <p>
+        Now in development, 
+        <a
+          target="_blank" rel="noopener noreferrer"
+          href="https://github.com/guilogar"
+        >
+          see my repositories
+        </a>
+      </p>
     </div>
   );
 };
